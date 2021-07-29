42137f03cbff600c1578bbb496a9eb909a38ec26
anushac96 <anushac96@gmail.com>
2021-07-29 20:13:05 +0530

    2nd way to rotate array

--- src/arrays/Prgm1.java
+++ src/arrays/Prgm1.java
@@ -13,12 +13,33 @@ public class Prgm1 {
 		// if d > a.len
 		d = d % a.length;
 		Prgm1 p1 = new Prgm1();
-		p1.rotate1(a, d); // time = d+a.len	space = d + a
+		//p1.rotate1(a, d); // time = d+a.len	space = d + a
+		p1.rotate12(a, d);	//time = d *(n-1)	space = 1
+	}
+
+	private void rotate12(int[] a, int d) {
+		if (d == 0 || d == a.length) {
+			System.out.println(Arrays.toString(a));
+		}
+		if(d<0) {
+			System.out.println("number is less than 0");
+		}
+		for(int i=0;i<d;i++) {
+			int temp = a[0];
+			for(int j=1;j<a.length;j++) {
+				a[j-1] = a[j];		//d *(n-1)
+			}
+			a[a.length-1] = temp;
+		}
+		System.out.println(Arrays.toString(a));
 	}
 
 	private void rotate1(int[] a, int d) {
 		if (d == 0 || d == a.length) {
 			System.out.println(Arrays.toString(a));
+		}
+		if(d<0) {
+			System.out.println("number is less than 0");
 		}
 		int temp[] = new int[d];
 		for (int i = 0; i < d; i++) {
