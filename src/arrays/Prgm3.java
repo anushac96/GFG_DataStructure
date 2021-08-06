2dbc3d0d4d3b7ae078d1618f620810fac8078274
anushac96 <anushac96@gmail.com>
2021-08-07 00:07:31 +0530

    find an element in rotated array

---
+++ src/arrays/Prgm3.java
@@ -0,0 +1,32 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Prgm3 {
+
+	public static void main(String[] args) {
+		int a[] = { 4, 5, 6, 7, 1, 2, 3 };
+		Prgm3 p1 = new Prgm3();
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter a number to search");
+		int k = scn.nextInt();
+		p1.search1(a, k);	// time = log(n) space = 1
+	}
+
+	private void search1(int[] a, int k) {
+		int start = 0;
+		int end = a.length - 1;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (k == a[mid]) {
+				System.out.println(k + " is found at " + mid + " index");
+				return ;
+			}
+			if (k >= a[start] && k < a[mid]) {
+				end = mid - 1;
+			} else
+				start = mid + 1;
+		}
+		System.out.print("key not found");
+	}
+}
--- src/arrays/ReadMe.txt
+++ src/arrays/ReadMe.txt
@@ -1,2 +1,3 @@
 Prgm1	->	https://www.geeksforgeeks.org/array-rotation/
 Prgm2	->	https://www.geeksforgeeks.org/c-program-cyclically-rotate-array-one/
+Prgm3	->	https://www.geeksforgeeks.org/search-an-element-in-a-sorted-and-pivoted-array/
