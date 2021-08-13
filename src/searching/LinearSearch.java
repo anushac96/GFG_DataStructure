254ccb4d33803097d07b0edf4274a3d163836f3e
anushac96 <anushac96@gmail.com>
2021-08-13 22:59:29 +0530

    improved version of linear search

--- src/searching/LinearSearch.java
+++ src/searching/LinearSearch.java
@@ -6,7 +6,27 @@ public class LinearSearch {
 		int a[] = { 10, 20, 80, 30, 60, 50, 110, 100, 130, 170 };
 		int key = 110;
 		LinearSearch l = new LinearSearch();
-		l.search1(a, key);
+		l.search1(a, key); // time complexity = O(n)
+		l.search2(a, key); // Improve Linear Search Worst-Case Complexity
+							// if element Found at last O(n) to O(1)
+							// if element Not found O(n) to O(n/2)
+	}
+
+	private void search2(int[] a, int key) {
+		int left = 0;
+		int right = a.length - 1;
+		while (left <= right) {
+			if (a[left] == key) {
+				System.out.println("found in " + left + " position");
+				break;
+			} else if (a[right] == key) {
+				System.out.println("found in " + right + " position");
+				break;
+			} else {
+				left++;
+				right--;
+			}
+		}
 	}
 
 	private void search1(int[] a, int key) {
