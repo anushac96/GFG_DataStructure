d42ab15c37d7c5fdee4999885b89aa234c7ae269
anushac96 <anushac96@gmail.com>
2021-10-29 02:50:59 +0530

    Find the middle of a given Singly LinkedList

---
+++ src/linkedList/Prgm1.java
@@ -0,0 +1,79 @@
+package linkedList;
+
+// Find the middle of a given linked list
+
+public class Prgm1 {
+	Node headNode;
+
+	public static void main(String[] args) {
+
+		Prgm1 prgm1 = new Prgm1();
+		prgm1.insert(1);
+		prgm1.insert(2);
+		prgm1.insert(3);
+		prgm1.insert(4);
+		prgm1.insert(5);
+		prgm1.insert(6);
+		prgm1.insert(7);
+		prgm1.insert(8);
+		prgm1.insert(9);
+		prgm1.insert(10);
+		prgm1.print();
+		System.out.println("mid element is: " + prgm1.findMid());
+
+	}
+
+	private int findMid() {
+		if (headNode == null) {
+			System.out.println("no element to find mid");
+		} else if (headNode.nextNode == null || headNode.nextNode.nextNode == null) {
+			return headNode.data;
+		} else {
+			Node midNode = headNode.nextNode;
+			Node tailNode = headNode.nextNode.nextNode;
+			while (tailNode.nextNode != null && tailNode.nextNode.nextNode != null) {
+				midNode = midNode.nextNode;
+				tailNode = tailNode.nextNode.nextNode;
+			}
+			return midNode.data;
+		}
+		return 0;
+	}
+
+	private void print() {
+		if (headNode == null) {
+			System.out.println("no element to print, list is empty");
+		} else {
+			Node curNode = headNode;
+			while (curNode.nextNode != null) {
+				System.out.print(curNode.data + " ");
+				curNode = curNode.nextNode;
+			}
+			System.out.println(curNode.data + " ");
+		}
+	}
+
+	public void insert(int data) {
+		Node newNode = new Node(data);
+		if (headNode == null) {
+			headNode = newNode;
+		} else {
+			Node curNode = headNode;
+			while (curNode.nextNode != null) {
+				curNode = curNode.nextNode;
+			}
+			curNode.nextNode = newNode;
+		}
+
+	}
+
+	public class Node {
+		int data;
+		Node nextNode;
+
+		public Node(int d) {
+			data = d;
+			nextNode = null;
+		}
+	}
+}
